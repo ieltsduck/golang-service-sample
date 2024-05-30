2b0550aa13b18d5ee42e4562d58f5cf715e71864
Andy Chen <chenyangpoppin@hotmail.com>
2024-05-30 17:42:23 +0800

    Adding /about route & apply logger middleware

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
 	app := fiber.New()
 
+	// Add logger middleware
+	app.Use(logger.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Go/Fiber Project!")
+		return c.SendString("Hello GoFiber Project!")
+	})
+
+	app.Get("/about", func(c *fiber.Ctx) error {
+		return c.SendString("About Page")
 	})
 
 	app.Listen(":3000")
